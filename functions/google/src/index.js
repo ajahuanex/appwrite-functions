5a4652a4647b24e1bc3dfc46ea35126ecbc03104
Ajahuana <ajahuana@hotmail.com>
2023-07-30 11:39:32 -0500

    insert code for api google

--- functions/google/src/index.js
+++ functions/google/src/index.js
@@ -1,4 +1,26 @@
 const sdk = require("node-appwrite");
+const path = require("path")
+
+const {google} = require('googleapis');
+
+const KEYFILEPATH = path.join(__dirname, "../assets/key.json");
+console.log('keyfile.....:', KEYFILEPATH);
+const SCOPES = ['https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.appdata',
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/drive.metadata',
+  'https://www.googleapis.com/auth/drive.metadata.readonly',
+  'https://www.googleapis.com/auth/drive.photos.readonly',
+  'https://www.googleapis.com/auth/drive.readonly',
+  'https://www.googleapis.com/auth/drive.scripts',
+  'https://www.googleapis.com/auth/drive.activity',
+  'https://www.googleapis.com/auth/drive.activity.readonly',
+  'https://www.googleapis.com/auth/documents'];
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: KEYFILEPATH,
+  scopes: SCOPES,
+});
 
 /*
   'req' variable has:
@@ -15,6 +37,30 @@ const sdk = require("node-appwrite");
 
 module.exports = async function (req, res) {
   const client = new sdk.Client();
+
+  const folderId='0AHiqxMoeTuR3Uk9PVA'
+  const service = google.drive({
+    version: 'v3',
+    auth: auth
+  });
+  const fileMetadata = {
+    name: Date.now().toString(),
+    mimeType: 'application/vnd.google-apps.folder',
+    parents: [folderId]
+  };
+  var iD='123'
+
+  try {
+    const folderExpediente = await service.files.create({
+      requestBody: fileMetadata,
+      fields: 'id,name',
+      supportsAllDrives: true,
+    })
+     iD = folderExpediente.data.id;
+    console.log(iD)
+  } catch (error) {
+    console.log(error)
+  }
 
   // You can remove services you don't use
   const account = new sdk.Account(client);
